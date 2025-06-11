062733f8582cf8d8ac0b4e5a63d44f8abefd768c
Michael <michaelkindye@gmail.com>
2025-06-11 21:33:38 +0300

    added admins' access for all features

--- booking_proj/book_app/permissions.py
+++ booking_proj/book_app/permissions.py
@@ -3,12 +3,12 @@ from rest_framework.permissions import BasePermission
 
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'customer'
+        return request.user.role == 'customer' or request.user.role == 'admin'
     
 class IsProvider(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'provider'
+        return request.user.role == 'provider' or request.user.role == 'admin'
     
 class HasServicePermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.provider == request.user
+        return obj.provider == request.user or request.user.role == 'admin'
